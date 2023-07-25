fedf79918e7f52cae130c80fceb95133fcee08e7
MinionaH <nikoloz.sadirini@gmail.com>
2023-07-25 16:01:11 +0400

    express go brr

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express();
 
-app.use((req, res, next)=> {
+app.use('/',(req, res, next)=> {
 
     console.log("in the middleware!")
     res.send('<h1>Hello from express!</h1>');
