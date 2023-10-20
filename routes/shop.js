246882257b274d5588324afff2e6fbd8fdfb11f6
MinionaH <nikoloz.sadirini@gmail.com>
2023-10-20 17:02:59 +0400

    EJS

--- routes/shop.js
+++ routes/shop.js
@@ -7,9 +7,16 @@ const router = express.Router();
 const rootDir = require('../utils/path')
 
 
-router.get('/',(req, res, next)=> {
-    console.log(adminData.products)
-    res.render('shop',{pageTitle:'Shop'});
+router.get('/', (req, res, next) => {
+    const products = adminData.products;
+    res.render('shop',{
+            pageTitle: 'Shop', 
+            prods: products,
+            path:'/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
 })
 
 
