6a18dc9701887a7cb5490a51375757954c7ed7a2
MinionaH <nikoloz.sadirini@gmail.com>
2023-10-24 18:47:17 +0400

    EJS

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ const products = [];
 
 router.get('/add-product',(req, res, next)=> {
 
-    res.render('add-product', {pageTitle:'Add product'});
+    res.render('add-product', {pageTitle:'Add product', path: '/add_product'});
 })
 
 
